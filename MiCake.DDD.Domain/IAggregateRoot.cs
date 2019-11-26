0da52fb10bac003859e90e2e8bfebd885c3a408f
uoyo <32832304+uoyoCsharp@users.noreply.github.com>
2019-11-26 17:43:29 +0800

    add ddd domian part

---
+++ MiCake.DDD.Domain/IAggregateRoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiCake.DDD.Domain
+{
+    /// <summary>
+    /// Defines an aggregate root. It's primary key may not be "Id" or it may have a composite primary key.
+    /// Use <see cref="IAggregateRoot{TKey}"/> where possible for better integration to repositories and other structures in the framework.
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+
+    /// <summary>
+    /// Defines an aggregate root with a single primary key with "Id" property.
+    /// </summary>
+    /// <typeparam name="TKey">Type of the primary key of the entity</typeparam>
+    public interface IAggregateRoot<TKey> : IEntity<TKey>, IAggregateRoot
+    {
+    }
+}
---
+++ MiCake.DDD.Domain/IValueObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiCake.DDD.Domain
+{
+    /// <summary>
+    /// Provide an interface to realize the value object of DDD
+    /// </summary>
+    public interface IValueObject
+    {
+    }
+}
