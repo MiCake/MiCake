0aa0f5b3f40f6f8f1021ea8d331591a32caf8b80
uoyo <32832304+uoyoCsharp@users.noreply.github.com>
2020-06-12 14:59:26 +0800

    :closed_umbrella: add module inspection.Fixed AB#50

---
+++ src/framework/MiCake.Core.Abstractions/Modularity/IModuleSelfInspection.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace MiCake.Core.Modularity
+{
+    /// <summary>
+    /// Defined that the module has self inspection function.
+    /// Self inspection will be carried out when the MiCake application is started.
+    /// </summary>
+    public interface IModuleSelfInspection
+    {
+        /// <summary>
+        /// Execute the logic of self inspection. If there is any error, throw the corresponding exception
+        /// </summary>
+        /// <param name="context"><see cref="ModuleInspectionContext"/></param>
+        void Inspect(ModuleInspectionContext context);
+    }
+}
